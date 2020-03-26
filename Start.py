380d29a92ff379da968ffb6e0a30009de04915fe
VoxelPrismatic <danielpetrov@outlook.com>
2020-03-25 20:27:09 -0500

    deleted:    Start.py

--- Start.py
+++
@@ -1,7 +0,0 @@
-import subprocess, time
-while True:
-    last = time.monotonic()
-    subprocess.run('python3.7 PRIZM.py'.split())
-    while time.monotonic() - last < 15:
-        print('CRASHED TOO SOON')
-        time.sleep(5)
